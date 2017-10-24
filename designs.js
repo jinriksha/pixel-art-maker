6e6633bef8649e7784504061d9b44edffcf99900
Richard Shaw <jinriksha@me.com>
2017-10-23 22:28:00 -0600

    Set variables for color and size inputs

--- designs.js
+++ designs.js
@@ -1,5 +1,9 @@
 // Select color input
+let color = $('#colorPicker').val();
+
 // Select size input
+let height = $('#input_height').val();
+let width = $('#input_width').val();
 
 // When size is submitted by the user, call makeGrid()
 
