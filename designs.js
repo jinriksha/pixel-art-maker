98e7137e32154a6b059f71b54aafd7a977e54ee5
Richard Shaw <jinriksha@me.com>
2017-10-23 22:38:03 -0600

    Added event listener for form submission

--- designs.js
+++ designs.js
@@ -7,8 +7,14 @@ let width = $('#input_width').val();
 
 // When size is submitted by the user, call makeGrid()
 
+$('sizePicker').submit(function(event) {
+  // Prevent default
+  event.preventDefault();
+
+  // Call the makeGrid function
+  makeGrid();
+});
+
 function makeGrid() {
-
-// Your code goes here!
-
+  
 }
